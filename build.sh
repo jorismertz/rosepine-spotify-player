71c5b877f3f8845c8205403764defe20b1a83ad9
Joris Mertz <info@jorismertz.nl>
2024-06-18 18:41:02 +0200

    remove bg to allow for transparency

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ npx @rose-pine/build -t template.toml
 rm -rf $THEME_FILE
 
 # Append contents in a combined file, formatted so it can be appended to the end of theme.toml
-echo "\n$(cat ./dist/rose-pine.toml)\n" >> $THEME_FILE
-echo "$(cat ./dist/rose-pine-moon.toml)\n" >> $THEME_FILE
+echo "$(cat ./dist/rose-pine.toml)" >> $THEME_FILE
+echo "$(cat ./dist/rose-pine-moon.toml)" >> $THEME_FILE
 cat ./dist/rose-pine-dawn.toml >> $THEME_FILE
